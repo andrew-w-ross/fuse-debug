04e963fd9ad609c629f5d3118d906d9a2a0e3770
Andrew Ross <andrew.ross@jembi.org>
2017-07-24 08:13:57 +0200

    Created debug version

--- fuse.js
+++ fuse.js
@@ -1,9 +1,13 @@
+const { path: rootPath } = require("app-root-path");
 const { FuseBox, WebIndexPlugin } = require("fuse-box");
+const path = require("path");
 
 const fuse = FuseBox.init({
 	homeDir: "src/",
 	output: "dist/$name.js",
-	sourceMaps: { sourceRoot: "src", inline: false },
+	modulesFolder: path.join(rootPath, "node_modules"),
+	sourceMaps: { sourceRoot: "src", inline: true },
+	debug:true,
 	plugins: [
 		WebIndexPlugin({ bundles: ["app"] })
 	]
