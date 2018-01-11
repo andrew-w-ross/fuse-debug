7df44c5329dd1430cde1f292f794ebe86742c768
Andrew Ross <andrew.ross@jembi.org>
2018-01-11 13:41:21 +0200

    Updated to fuse 3 Config is quite a bit simpler Issue with source map update.

--- fuse.js
+++ fuse.js
@@ -5,8 +5,7 @@ const path = require("path");
 const fuse = FuseBox.init({
 	homeDir: "src/",									
 	output: "dist/$name.js",
-	modulesFolder: path.join(rootPath, "node_modules"), //Only needed if you are globally linking for dev purposes
-	sourceMaps: { sourceRoot: "src", inline: false }, //By default the sourceRoot is /src this generally breaks the vscode debugger	
+	sourceMaps: { inline: false },
 	plugins: [
 		WebIndexPlugin({ bundles: ["app"] })
 	]
@@ -16,6 +15,7 @@ fuse.dev({ httpServer: true, hmr: true });
 
 fuse.bundle("app")
 	.instructions(">index.tsx")
+	.target("browser@es2015")
 	.watch()
 	.hmr();	
 
