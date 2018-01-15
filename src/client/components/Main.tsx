c797d7324cf435183b73f46ae71552a234403add
Andrew Ross <andrew.ross@kurtosys.com>
2018-01-16 00:06:19 +0200

    Changed order

--- src/client/components/Main.tsx
+++ src/client/components/Main.tsx
@@ -8,9 +8,9 @@ export class Main extends React.Component {
 		return (
 			<div>
 				<h1>Hello World</h1>
-				<Adder />
-				<hr/>				
 				<ServerCaps />
+				<hr/>				
+				<Adder />
 			</div>
 		);
 	}
